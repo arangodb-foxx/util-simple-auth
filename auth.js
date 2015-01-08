f0b0f1780776ed2166f265e526ba807bbf4b2591
Alan Plum <me@pluma.io>
2015-01-08 14:38:07 +0100

    Initial commit.

---
+++ auth.js
@@ -0,0 +1,28 @@
+/*global require, exports, applicationContext */
+(function () {
+  'use strict';
+  var crypto = require('org/arangodb/crypto'),
+    cfg = applicationContext.configuration;
+
+  function verifyPassword(authData, password) {
+    if (!authData) {
+      authData = {};
+    }
+    var hashMethod = authData.method || cfg.hashMethod,
+      salt = authData.salt || '',
+      storedHash = authData.hash || '',
+      generatedHash = crypto[hashMethod](salt + password);
+    // non-lazy comparison to avoid timing attacks
+    return crypto.constantEquals(storedHash, generatedHash);
+  }
+
+  function hashPassword(password) {
+    var hashMethod = cfg.hashMethod,
+      salt = crypto.genRandomAlphaNumbers(cfg.saltLength),
+      hash = crypto[hashMethod](salt + password);
+    return {method: hashMethod, salt: salt, hash: hash};
+  }
+
+  exports.verifyPassword = verifyPassword;
+  exports.hashPassword = hashPassword;
+}());
